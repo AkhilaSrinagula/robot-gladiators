f48dc667b13629b1feaf7d64fafbd377441a31b9
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-09-25 21:25:37 -0500

    add pickedEnemyName

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
     // if player choses to fight, then fight
@@ -59,6 +60,9 @@ var fight = function(enemyName) {
     window.alert("You need to choose a valid option. Try again!");
   }
   };
+  }
+    
+    
 
   for(var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i]);
@@ -66,7 +70,9 @@ var fight = function(enemyName) {
     console.log(enemyNames[i] + " is at " + i + " index");
   }
 
-  for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+  for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
 
